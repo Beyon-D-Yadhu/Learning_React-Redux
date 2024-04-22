9057c03ba7537afa246d10b509d7a53bdc636beb
Yadhu-krishnan <yadhuy28@gmail.com>
2024-04-23 00:49:03 +0530

    studied how to use useDispatch hook

--- src/components/HooksCakeContainer.jsx
+++ src/components/HooksCakeContainer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { buyCake } from '../redux/cake/cakeActions'
 
 function HooksCakeContainer() {
     const numOfCakes = useSelector(state => state.numOfCakes)
+    const dispatch = useDispatch() //returns a reference to dispatch function from redux store
   return (
     <div>
         <h2>Num of cakes - {numOfCakes}</h2>
-        <button>By cake</button>
+        <button onClick={()=>dispatch(buyCake())}>By cake</button>
     </div>
   )
 }
